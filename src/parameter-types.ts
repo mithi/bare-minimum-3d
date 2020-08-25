d68005810b4de568de6d1930b1c34d00af36c009
Mithi <mithi.sevilla@gmail.com>
2020-08-26 03:29:28 +0800

    feat: Add parameter types

---
+++ src/parameter-types.ts
@@ -0,0 +1,46 @@
+interface ViewSettings {
+    camTx: number
+    camTy: number
+    camTz: number
+    camZoom: number
+    canvasToViewRatio: number
+    cubeRx: number
+    cubeRy: number
+    cubeRz: number
+    defaultCamOrientation: string
+    defaultCamZoffset: number
+}
+
+interface SceneSettings {
+    cubeRange: number
+    cubeZoffset: number
+    dataZoffset: number
+    paperXrange: number
+    paperYrange: number
+}
+
+interface SceneOptions {
+    paper: ModelOptions
+    xyPlane?: ModelOptions
+    sceneEdges?: ModelOptions
+    crossLines?: ModelOptions
+    edgeAxes?: AxesOptions
+    worldAxes?: AxesOptions
+    cubeAxes?: AxesOptions
+}
+
+interface ModelOptions {
+    color: string
+    opacity?: number
+}
+
+interface AxesOptions {
+    intersectionPointColor: string
+    intersectionPointSize: number
+    xEdgeColor: string
+    yEdgeColor: string
+    zEdgeColor: string
+    edgeOpacity?: number
+}
+
+export { ViewSettings, SceneSettings, SceneOptions }
