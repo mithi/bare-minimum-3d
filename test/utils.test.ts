27b41a877c967c11d76902f460598627aa15ae69
Mithi <mithi.sevilla@gmail.com>
2020-08-30 17:19:53 +0800

    move: Rename test data

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,8 +1,8 @@
 import { renderScene } from "../src/utils"
 import { viewSettings, sceneSettings } from "./test-settings"
-import { sceneOptions } from "./test-scene-options"
+import { sceneOptions } from "./test-input-scene-options"
 
-import hexapodData from "./test-data-3d"
+import hexapodData from "./test-input-data-3d"
 test("render scene", () => {
     expect(true).toBe(true)
     renderScene(viewSettings, sceneSettings, sceneOptions, hexapodData)
